ef0647f157ea0fb431853d6533eee076a13ad159
Hermit <alex_dc3@mail.ru>
2025-03-24 12:01:29 +0100

    Refactor code

--- src/components/Data.jsx
+++ src/components/Data.jsx
@@ -7,23 +7,25 @@ const Data = () => {
     const [weatherInfo, setWeatherInfo] = useState({});
     const [message, setMessage] = useState("Введите название города");
 
-    const getWeather = city => {
-        fetch(`${base_url}?q=${city}&appid=${api_key}&units=metric`)
-            .then(res => res.json()) // возвращает промис
-            .then(data => {
-                setWeatherInfo({
-                    city: data.name,
-                    country: data.sys.country,
-                    temperature: data.main.temp,
-                    pressure: data.main.pressure,
-                    sunset: data.sys.sunset,
-                    wind: data.wind.speed,
-                    clouds: data.clouds.all,
-                    humidity: data.main.humidity
-                });
-                setMessage('');
-            })
-        .catch(() => setMessage('Город не найден, введите корректное название города'));
+    const getWeather = async (city) => {
+        try {
+            const res = await fetch(`${base_url}?q=${city}&appid=${api_key}&units=metric`);
+            const data = await res.json();
+            setWeatherInfo({
+                city: data.name,
+                country: data.sys.country,
+                temperature: data.main.temp,
+                pressure: data.main.pressure,
+                sunset: data.sys.sunset,
+                wind: data.wind.speed,
+                clouds: data.clouds.all,
+                humidity: data.main.humidity
+            });
+            setMessage('');
+        } catch (e) {
+            console.log(e)
+            setMessage('Город не найден, введите корректное название города');
+        }
     }
 
     return (
@@ -32,6 +34,7 @@ const Data = () => {
             <Weather weather={weatherInfo} message={message}/>
         </div>
     );
+
 };
 
 export default Data;
--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,19 +1,18 @@
-import React from 'react';
-
 const Weather = ({weather, message}) => {
-    if(message) return <div className={'InfoWeath'}>{message}</div>;
+
     return (
-        <div className={'InfoWeath'}>
-            <p>Location: {weather.city}</p>
-            <p>Country: {weather.country}</p>
-            <p>Temperature: {weather.temperature}</p>
-            <p>Pressure: {weather.pressure}</p>
-            <p>Sunset: {(new Date (weather.sunset * 1000)).toLocaleTimeString()}</p>
-            <p>Wind: {weather.wind}</p>
-            <p>Clouds: {weather.clouds}</p>
-            <p>Humidity: {weather.humidity}</p>
+        <div className={'infoWeath'}>
+            {!message &&
+                <>
+                    <p>Location: {weather.country}, {weather.city}</p>
+                    <p>Temp: {weather.temp}</p>
+                    <p>Pressure: {weather.pressure}</p>
+                    <p>Sunset: {(new Date(weather.sunset * 1000)).toLocaleTimeString()}</p>
+                </>
+            }
+            {message}
         </div>
-    );
-};
+    )
+}
 
 export default Weather;
